bf3c289aaf322eaab967203a1513d64ec74e247b
Prabhat <prabhatkumarverma42@gmail.com>
2025-10-05 22:56:50 +0530

    feat(validators-and-types): Created a new package @repo/validators-and-types
    - Added registerSchema and loginSchema using Zod for user authentication
    - Validates email, password, and name for registration and login
    - add auth validation schemas

---
+++ packages/validators-and-types/validators/auth.validators.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+// packages/validators-and-types/validators/auth.validators.ts
+
+export const registerSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.email({ message: "Invalid email address" }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+export const loginSchema = z.object({
+  email: z.email({ message: "Invalid email address" }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+});
