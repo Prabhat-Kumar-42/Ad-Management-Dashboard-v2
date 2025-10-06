cb5bd5ba608012493296599c322b42956d3bd40f
Prabhat <prabhatkumarverma42@gmail.com>
2025-10-06 10:27:12 +0530

    fix: update user type

--- apps/backend/src/types/auth-request.ts
+++ apps/backend/src/types/auth-request.ts
@@ -3,5 +3,5 @@ import type { tokenalizedUserData } from "./tokenalized-user-data.type.js";
 
 // /src/types/AuthRequest.ts
 export interface AuthRequest extends Request {
-  user?: { id: string };
+  user?: tokenalizedUserData;
 }
