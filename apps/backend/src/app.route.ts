2525fe1a2b66b357094d9db3f195ae58d64bebc6
Prabhat <prabhatkumarverma42@gmail.com>
2025-10-11 12:52:39 +0530

    feat( backend ): wired up meta router

--- apps/backend/src/app.route.ts
+++ apps/backend/src/app.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { authRouter } from "./modules/auth/auth.route.js";
+import { metaRouter } from "./modules/providers/meta/meta.route.js";
 
 // /src/app.route.js
 export const appRouter: Router = Router();
 
 appRouter.use("/auth", authRouter);
+appRouter.use("/meta", metaRouter);
