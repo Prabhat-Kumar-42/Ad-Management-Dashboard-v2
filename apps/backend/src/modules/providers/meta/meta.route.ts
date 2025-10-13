9eab71a86564a785af4f32c8bf4e96d3a6d4b21f
Prabhat <prabhatkumarverma42@gmail.com>
2025-10-13 14:48:59 +0530

    refactor(meta): modularize Meta routes into OAuth and campaign sub-routers
    - Moved OAuth-related routes into metaOauthConnectRouter
    - Moved campaign-related routes into metaCampaignRouter
    - Updated metaRouter to delegate to sub-routers for better separation of concerns

--- apps/backend/src/modules/providers/meta/meta.route.ts
+++ apps/backend/src/modules/providers/meta/meta.route.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { disconnectMetaAccount, getAccounts, getMetaAuthUrl, handleMetaCallback } from "./meta.controller.js";
-import { authMiddleware } from "@/middlewares/authenticate.middleware.js";
+import { metaOauthConnectRouter } from "./oauth/metaOauth.route.js";
+import { metaCampaignRouter } from "./campaign/metaCampaign.route.js";
 
-// /src/modules/providers/meta/meta.route.ts
+// /src/modules/providers/meta/meta.route.js
 export const metaRouter: Router = Router();
 
-metaRouter.get("/auth/url", authMiddleware, getMetaAuthUrl);
-metaRouter.get("/callback", authMiddleware, handleMetaCallback);
-metaRouter.get("/accounts", authMiddleware, getAccounts);
-metaRouter.delete("/deauth", authMiddleware, disconnectMetaAccount);
+metaRouter.use('/', metaOauthConnectRouter);
+metaRouter.use('/campaigns', metaCampaignRouter);
